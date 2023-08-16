f4451a5b7e2a9012c910c10445126fd44685645a
rajnishramaera <rajnish.it@ramaera.in>
2023-08-16 12:50:20 +0530

    Button component

--- storybook-static/assets/preview-410e3fc8.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-06184bc1.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-54d41a2b.js"),["./DocsRenderer-3PUGWF3O-54d41a2b.js","./iframe-06184bc1.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./react-18-ba2f8a3a.js","./index-6a5bd4ef.js","./index-37c51057.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-410e3fc8.js.map
